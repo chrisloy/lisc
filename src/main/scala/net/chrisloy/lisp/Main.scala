8c6a99502f1343821ce558c292d8bbeebc881476
Chris Loy <chrisloyuk@gmail.com>
2014-12-13 20:31:28 +0000

    Added a simple REPL

---
+++ src/main/scala/net/chrisloy/lisp/Main.scala
@@ -0,0 +1,27 @@
+package net.chrisloy.lisp
+
+object Main extends App {
+
+  implicit val scope = new Scope
+
+  val parse = new Parser
+  val prompt = "lisc> "
+
+  println(
+    """
+      |Welcome to LiSc!
+      |This is a toy Lisp interpreter, written in Scala.
+      |Type :q to leave
+    """.stripMargin)
+
+  print(prompt)
+
+  for (ln <- io.Source.stdin.getLines().takeWhile(_ != ":q")) {
+    if (ln.nonEmpty) {
+      val eval = parse(ln).value
+      println(s"==> $eval")
+    }
+    print(prompt)
+  }
+}
+
