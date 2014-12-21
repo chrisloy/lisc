076fe7925fa317a96ea03a820866f4614701c3ce
Chris Loy <chrisloyuk@gmail.com>
2014-12-21 17:49:27 +0000

    Implemented -, <, ! and &&

--- src/main/scala/net/chrisloy/lisc/BuiltIns.scala
+++ src/main/scala/net/chrisloy/lisc/BuiltIns.scala
@@ -5,14 +5,35 @@ object BuiltIns {
   private type Args = List[Expression]
 
   val all: Map[Symbol, Eval] = Map(
-    Symbol("=") -> { implicit scope => _eq _ },
-    Symbol("+") -> { implicit scope => _add _ }
+    Symbol("=")  -> { implicit scope => _eq _ },
+    Symbol("+")  -> { implicit scope => _add _ },
+    Symbol("-")  -> { implicit scope => _minus _ },
+    Symbol("<")  -> { implicit scope => _lt _ },
+    Symbol("!")  -> { implicit scope => _not _ },
+    Symbol("&&") -> { implicit scope => _and _}
   )
 
-  def _eq(args: Args)(implicit scope: Scope): Any = args map (_.value) reduce (_ == _)
+  def _eq(args: Args)(implicit scope: Scope): Value = args map (_.value) reduce (_ == _)
 
-  def _add(args: Args)(implicit scope: Scope): Any = args match {
+  def _add(args: Args)(implicit scope: Scope): Value = args match {
     case a if a.forall(_.isLong) => a.map(_.toLong).reduce(_ + _)
     case a => a map (_.toDouble) reduce (_ + _)
   }
+
+  def _minus(args: Args)(implicit scope: Scope): Value = args match {
+    case a if a.forall(_.isLong) => a.map(_.toLong).reduce(_ - _)
+    case a => a map (_.toDouble) reduce (_ - _)
+  }
+
+  def _lt(args: Args)(implicit scope: Scope): Value = args match {
+//   TODO make variadic
+    case List(x, y) if x.isLong && y.isLong => x.toLong < y.toLong
+    case List(x, y) => x.toDouble < y.toDouble
+  }
+
+  def _not(args: Args)(implicit scope: Scope): Value = args match {
+    case List(x) => !x.toBoolean
+  }
+
+  def _and(args: Args)(implicit scope: Scope) = args.forall(_.toBoolean)
 }
--- src/test/scala/net/chrisloy/lisc/LibrarySpec.scala
+++ src/test/scala/net/chrisloy/lisc/LibrarySpec.scala
@@ -9,7 +9,7 @@ class LibrarySpec extends FlatSpec with MustMatchers with Checkers {
 
   val parse = new Parser
 
-  "(+)" should "add two integers" in {
+  "(+ ...)" should "add two integers" in {
     check((x: Long, y: Long) => parse(s"(+ $x $y)").value == x + y)
   }
 
@@ -21,7 +21,7 @@ class LibrarySpec extends FlatSpec with MustMatchers with Checkers {
     check((x: Long, y: Long, z: Long) => parse(s"(+ $x (+ $y (+ $z)))").value == x + y + z)
   }
 
-  "(=)" should "handle numbers" in {
+  "(= ...)" should "handle numbers" in {
     check((x: Long) => parse(s"(= $x $x)").value == true)
     check((x: Long, y: Long) => parse(s"(= $x $y)").value == (x == y))
   }
@@ -30,5 +30,31 @@ class LibrarySpec extends FlatSpec with MustMatchers with Checkers {
     parse(s"""(= "" "")""").value mustBe true
     parse(s"""(= "pies" "pies")""").value mustBe true
     parse(s"""(= "pies" "cakes")""").value mustBe false
+  }
+
+  "(< ...)" should "compare correctly" in {
+    check((x: Long, y: Long) => parse(s"(< $x $y)").value == (x < y))
+    check((x: Long, y: Double) => parse(s"(< $x $y)").value == (x < y))
+    check((x: Double, y: Long) => parse(s"(< $x $y)").value == (x < y))
+    check((x: Double, y: Double) => parse(s"(< $x $y)").value == (x < y))
+  }
+
+  "(- ...)" should "subtract correctly" in {
+    check((x: Long, y: Long) => parse(s"(- $x $y)").value == (x - y))
+    check((x: Long, y: Double) => parse(s"(- $x $y)").value == (x - y))
+    check((x: Double, y: Long) => parse(s"(- $x $y)").value == (x - y))
+    check((x: Double, y: Double) => parse(s"(- $x $y)").value == (x - y))
+  }
+
+  "(! ...)" should "invert booleans" in {
+    parse("(! true)").value mustBe false
+    parse("(! false)").value mustBe true
+  }
+
+  "(&& ...)" should "return true only if all arguments are booleans" in {
+    parse("(&& true)").value mustBe true
+    parse("(&& false)").value mustBe false
+    parse("(&& true true true true)").value mustBe true
+    parse("(&& true true false true)").value mustBe false
   }
 }
