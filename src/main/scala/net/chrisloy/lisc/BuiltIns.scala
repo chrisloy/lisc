777f7f07b14bd3ec00b5168137209f294b9c36a3
Chris Loy <chrisloyuk@gmail.com>
2014-12-23 17:31:18 +0000

    Multiplication and division

--- src/main/scala/net/chrisloy/lisc/BuiltIns.scala
+++ src/main/scala/net/chrisloy/lisc/BuiltIns.scala
@@ -8,6 +8,8 @@ object BuiltIns {
     Symbol("=")  -> { implicit scope => _eq _ },
     Symbol("+")  -> { implicit scope => _add _ },
     Symbol("-")  -> { implicit scope => _minus _ },
+    Symbol("*")  -> { implicit scope => _multiply _ },
+    Symbol("/")  -> { implicit scope => _divide _ },
     Symbol("<")  -> { implicit scope => _lt _ },
     Symbol("!")  -> { implicit scope => _not _ },
     Symbol("&&") -> { implicit scope => _and _}
@@ -23,6 +25,16 @@ object BuiltIns {
   def _minus(args: Args)(implicit scope: Scope): Value = args match {
     case a if a.forall(scope.isLong) => a.map(scope.toLong).reduce(_ - _)
     case a => a.map(scope.toDouble) reduce (_ - _)
+  }
+
+  def _multiply(args: Args)(implicit scope: Scope): Value = args match {
+    case a if a.forall(scope.isLong) => a.map(scope.toLong).reduce(_ * _)
+    case a => a.map(scope.toDouble) reduce (_ * _)
+  }
+
+  def _divide(args: Args)(implicit scope: Scope): Value = args match {
+    case a if a.forall(scope.isLong) => a.map(scope.toLong).reduce(_ / _)
+    case a => a.map(scope.toDouble) reduce (_ / _)
   }
 
   def _lt(args: Args)(implicit scope: Scope): Value = args match {
--- src/test/scala/net/chrisloy/lisc/LibrarySpec.scala
+++ src/test/scala/net/chrisloy/lisc/LibrarySpec.scala
@@ -3,6 +3,8 @@ package net.chrisloy.lisc
 import org.scalatest.prop.Checkers
 import org.scalatest.{MustMatchers, FlatSpec}
 
+import scala.util.control.NonFatal
+
 class LibrarySpec extends FlatSpec with MustMatchers with Checkers {
 
   implicit val scope = Scope()
@@ -64,5 +66,22 @@ class LibrarySpec extends FlatSpec with MustMatchers with Checkers {
     eval(parse("(&& false)")) mustBe false
     eval(parse("(&& true true true true)")) mustBe true
     eval(parse("(&& true true false true)")) mustBe false
+  }
+
+  "(* ...)" should "work" in {
+    eval(parse("(* 1 2 3 4 5)")) mustBe 120
+    check((x: Long, y: Long) => eval(parse(s"(* $x $y)")) == (x * y))
+    check((x: Long, y: Double) => eval(parse(s"(* $x $y)")) == (x * y))
+    check((x: Double, y: Long) => eval(parse(s"(* $x $y)")) == (x * y))
+    check((x: Double, y: Double) => eval(parse(s"(* $x $y)")) == (x * y))
+  }
+
+  "(/ ...)" should "work" in {
+    eval(parse("(/ 1000 10 5)")) mustBe 20
+    def mustBeZero(y: Any): PartialFunction[Throwable, Boolean] = { case NonFatal(ex) => y == 0 }
+    check((x: Long, y: Long) => try eval(parse(s"(/ $x $y)")) == (x / y) catch mustBeZero(y))
+    check((x: Long, y: Double) => try eval(parse(s"(/ $x $y)")) == (x / y) catch mustBeZero(y))
+    check((x: Double, y: Long) => try eval(parse(s"(/ $x $y)")) == (x / y) catch mustBeZero(y))
+    check((x: Double, y: Double) => try eval(parse(s"(/ $x $y)")) == (x / y) catch mustBeZero(y))
   }
 }
